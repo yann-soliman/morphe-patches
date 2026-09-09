764ae83a017aaf8b8e9f5210eacbe9896e8e81ff
yann-soliman <22856901+yann-soliman@users.noreply.github.com>
2026-09-09 10:06:40 +0200

    build: clean before semantic release publish

--- patches/build.gradle.kts
+++ patches/build.gradle.kts
@@ -39,11 +39,17 @@ patches {
 }
 
 tasks {
+    named("build") {
+        mustRunAfter("clean")
+    }
+
     register<JavaExec>("generatePatchesList") {
         description = "Build patch with patch list"
         dependsOn(build)
         classpath = sourceSets["main"].runtimeClasspath + patchListGeneratorClasspath
         mainClass.set("util.PatchListGeneratorKt")
     }
-    publish { dependsOn("generatePatchesList") }
+    publish {
+        dependsOn("clean", "generatePatchesList")
+    }
 }
